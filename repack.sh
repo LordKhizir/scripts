1a87c6ef72f897f803bbbdeee45050964c7e26b8
Toni Navarro <lordkhizir@gmail.com>
2020-12-09 20:27:37 +0100

    Create repack.sh

---
+++ repack.sh
@@ -0,0 +1,15 @@
+#Repack
+#For each subfolder in current folder, extrar RAR files, remove them, and repack all content as .7z
+#TODO: Expand with support for .zip - YAGNI
+dir=pwd
+for folder in */
+  do cd "$folder"
+  pwd
+  for rarfile in *.rar
+    do unrar x "$rarfile"
+    rm "$rarfile"
+  done
+  cd "$dir"
+  7za a -r -t7z -mx=9 "${folder%/}.7z" "$folder"
+  rm -r "$folder"
+done
