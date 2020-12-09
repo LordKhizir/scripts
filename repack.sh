139b2a12fceaf01a088b0fb433f0e383cce09cbf
Toni Navarro <lordkhizir@gmail.com>
2020-12-09 20:46:27 +0100

    Update repack.sh

--- repack.sh
+++ repack.sh
@@ -1,10 +1,10 @@
 #Repack
 #For each subfolder in current folder, extrar RAR files, remove them, and repack all content as .7z
 #TODO: Expand with support for .zip - YAGNI
-dir=pwd
+dir=`pwd`
 for folder in */
   do cd "$folder"
-  pwd
+  echo "Processing folder [$folder]"
   for rarfile in *.rar
     do unrar x "$rarfile"
     rm "$rarfile"
