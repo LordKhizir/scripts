02d9b3033e4af33252f51eae5cc5bf836cf39429
Toni Navarro <lordkhizir@gmail.com>
2020-12-23 18:34:55 +0100

    Update unpack.sh
    Work ONLY on current folder

--- unpack.sh
+++ unpack.sh
@@ -1,34 +1,29 @@
 #Unpack
-#For each subfolder in current folder, extrar RAR and ZIP files, and remove them
+#Extrar RAR and ZIP files, and remove them
 
 set -e #Halt on error, to avoid removing unprocessed files
 dir=`pwd`
-for folder in */
-  do cd "$folder"
-  echo "Processing folder [$folder]"
-  for rarfile in *.rar
-    do
-       dirname=$(echo "$rarfile" | sed -e 's/\.[^.]*$//')
-       if [ "${dirname}" == "*" ] #Avoid processing * in case of no rar files
-       then
-         continue
-       fi
-       echo "Processing RAR file [$rarfile]"
-       mkdir "$dirname"
-       unrar x -r "$rarfile" "$dirname"/
-       rm "$rarfile"
-    done
-  for zipfile in *.zip
-    do
-      dirname=$(echo "$zipfile" | sed -e 's/\.[^.]*$//')
-      if [ "${dirname}" == "*" ] #Avoid processing * in case of no rar files
-      then
-        continue
-      fi
-      echo "Processing ZIP file [$zipfile]"
-      mkdir "$dirname"
-      unzip "$zipfile" -d "$dirname"
-      rm "$zipfile"
-    done
-  cd "$dir"
-done
+for rarfile in *.rar
+  do
+     dirname=$(echo "$rarfile" | sed -e 's/\.[^.]*$//')
+     if [ "${dirname}" == "*" ] #Avoid processing * in case of no rar files
+     then
+       continue
+     fi
+     echo "Processing RAR file [$rarfile]"
+     mkdir "$dirname"
+     unrar x -r "$rarfile" "$dirname"/
+     rm "$rarfile"
+  done
+for zipfile in *.zip
+  do
+    dirname=$(echo "$zipfile" | sed -e 's/\.[^.]*$//')
+    if [ "${dirname}" == "*" ] #Avoid processing * in case of no rar files
+    then
+      continue
+    fi
+    echo "Processing ZIP file [$zipfile]"
+    mkdir "$dirname"
+    unzip "$zipfile" -d "$dirname"
+    rm "$zipfile"
+  done
