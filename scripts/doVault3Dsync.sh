66c01d0971a3de975be14ee324e733760df643a9
Toni Navarro <toni.navarro@infojobs.net>
2021-01-23 13:45:11 +0100

    Folder selection on rsync

--- scripts/doVault3Dsync.sh
+++ scripts/doVault3Dsync.sh
@@ -3,7 +3,7 @@
 #
 
 #halt on error
-set -e
+#set -e
 
 #including authentication
 source ${0%/*}/../secrets/Vault3D_THERE_credentials.sh
@@ -37,7 +37,7 @@ copyFromHereToThere() {
   echo "----------------------------------------"
   echo "Starting rsync from ${HereBaseFolder} to ${ThereBaseFolder}"
   date
-  rsync -avut --progress --exclude=.DS_Store --exclude=._* ${HereBaseFolder}/Vault3D/ ${ThereBaseFolder}/Vault3D/
+  rsync -avut --progress --timeout=0 --exclude=.DS_Store --exclude=._* "${HereBaseFolder}/Vault3D/${folderToSync}" "${ThereBaseFolder}/Vault3D/${folderToSync}"
 }
 
 # Copy from there to here
@@ -46,21 +46,32 @@ copyFromThereToHere() {
   echo "----------------------------------------"
   echo "Starting rsync from ${ThereBaseFolder} to ${HereBaseFolder}"
   date
-  rsync -avut --progress --exclude=.DS_Store --exclude=._* ${ThereBaseFolder}/Vault3D/ ${HereBaseFolder}/Vault3D/
+  rsync -avut --progress --timeout=0 --exclude=.DS_Store --exclude=._* "${ThereBaseFolder}/Vault3D/${folderToSync}" "${HereBaseFolder}/Vault3D/${folderToSync}"
 }
 
 #Usage
 display_usage() {
   echo "Automate Vault3D remote sync\n"
-  echo "Usage: $0 [operation]\n"
+  echo "Configured to always update, but never delete. Also excludes usual noise files (like ._wtf, .DS_Store)"
+  echo "Usage: ${0##*/} operation [folder]\n"
   echo "[operation] can be one of:"
   echo "\tmount\tMount remote point THERE"
   echo "\tup\tCopy from HERE to THERE"
   echo "\tdown\tCopy from THERE to HERE"
   echo "\tfull\tFirst copy from HERE to THERE, and then from THERE to HERE"
   echo "\thelp\tShow this message"
-  echo "All sync commands (up, down, full) will first mount remote THERE if needed"
+  echo 'All sync commands {up, down, full} will first mount remote THERE if needed'
+  echo 'If a folder is provided, only those folder contents will be synced'
 }
+
+
+# Get selected folder (if any)
+if [ "$2" == "" ];
+then
+  folderToSync=""
+else
+  folderToSync="${2}/"
+fi
 
 case $1 in
   mount)
