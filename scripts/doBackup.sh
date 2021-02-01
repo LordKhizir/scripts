dbd93debd027775ca5e265182948c3df28613c74
Toni Navarro <toni.navarro@infojobs.net>
2021-02-01 23:40:45 +0100

    Improvement on backup scripts - support for diferential compare

--- scripts/doBackup.sh
+++ scripts/doBackup.sh
@@ -1,1 +1,1 @@
-rsync -v --archive --progress --human-readable --backup --backup-dir=OLD-FILES-TO-REMOVE --exclude=.DS_Store --exclude=._* --delete ~/NAS/3D/ ~/Backup/3D
+rsync -v --archive --progress --timeout=0 --human-readable --backup --backup-dir=OLD-FILES-TO-REMOVE --exclude=.DS_Store --exclude=._* --delete ~/NAS/Vault3D/ ~/Backup/Vault3D
--- scripts/doVault3Dsync.sh
+++ scripts/doVault3Dsync.sh
@@ -28,7 +28,46 @@ mountRemote() {
   then
       echo "Mounting remote"
       mount_smbfs ${THERE_mount_string} ${ThereBaseFolder}
-    fi
+  fi
+}
+
+setTotalTime() {
+  TIME_IN_MINUTES=$((((($DAYS * 24) + $HOURS) * 60) + $MINUTES))
+  if [ "${TIME_IN_MINUTES}" == "0" ]
+  then
+    TIME_IN_MINUTES=1440 # 24h * 60m, default 1 day
+  fi
+}
+
+# Find differences on the last n hours
+findDifferences() {
+  #TODO OJO puede ser que me haya cargado KLT Studio con los sed... bajarlo
+  mountRemote
+  setTotalTime
+  echo "----------------------------------------"
+  echo "Starting diff from files modified in last N hours"
+  date
+  echo "Finding files modified on last ${TIME_IN_MINUTES} minutes in ${HereBaseFolder}/Vault3D/${FOLDER} -- written on recent_files_Here_unsorted.txt"
+  find "${HereBaseFolder}/Vault3D/${FOLDER}" -not -name ".DS_Store" -not -name "\._*" -mmin -${TIME_IN_MINUTES} \
+    | sed 's/\/\//\//' \
+    | sed 's/.*Vault3D//' \
+    > recent_files_Here_unsorted.txt
+  echo "Finding files modified on last ${TIME_IN_MINUTES} minutes in ${ThereBaseFolder}/Vault3D/${FOLDER} -- written on recent_files_There_unsorted.txt"
+  find "${ThereBaseFolder}/Vault3D/${FOLDER}"  -not -name ".DS_Store" -not -name "\._*" -mmin -${TIME_IN_MINUTES} \
+    | sed 's/\/\//\//' \
+    | sed 's/.*Vault3D//' \
+    > recent_files_There_unsorted.txt
+  echo "Sorting files"
+  sort < recent_files_Here_unsorted.txt > recent_files_Here.txt
+  sort < recent_files_There_unsorted.txt > recent_files_There.txt
+  rm recent_files_*_unsorted.txt
+  echo "--- Differences ------------------------"
+  echo "< 'Here'                                                                      > 'There' >"
+  diff --side-by-side --width=160 recent_files_Here.txt recent_files_There.txt
+}
+
+findDifferences2() {
+  echo "MINUTES ${MINUTES}"
 }
 
 # Copy from here to there
@@ -53,12 +92,13 @@ copyFromThereToHere() {
 display_usage() {
   echo "Automate Vault3D remote sync\n"
   echo "Configured to always update, but never delete. Also excludes usual noise files (like ._wtf, .DS_Store)"
-  echo "Usage: ${0##*/} {-up|-down|-full|-mount} [-f|--folder=FOLDER] [-d|--dry-run] [-h|--help]\n"
+  echo "Usage: ${0##*/} {-up|-down|-full|-mount|-diff} [--days] [--hours] [--minutes] [-f|--folder=FOLDER] [-d|--dry-run] [-h|--help]\n"
   echo "Main operation:"
   echo "\t-mount\tMount remote point THERE"
   echo "\t-up\tCopy from HERE to THERE"
   echo "\t-down\tCopy from THERE to HERE"
   echo "\t-full\tFirst copy from HERE to THERE, and then from THERE to HERE"
+  echo "\t-diff\tGet a side-by-side list of files recently modified. If days, hours and/or minutes are informed, time is calculated with them. If not, time will be 24h"
   echo "\t-help\tShow this message"
   echo "Options"
   echo '\tAll sync commands {up, down, full} will first mount remote THERE if needed'
@@ -70,6 +110,9 @@ display_usage() {
 METHOD="help"
 FOLDER=""
 DRYRUN=""
+MINUTES="0"
+HOURS="0"
+DAYS="0"
 
 for i in "$@"
 do
@@ -90,16 +133,32 @@ case $i in
     METHOD="mount"
     shift
     ;;
+    -diff)
+    METHOD="diff"
+    shift
+    ;;
+    --help)
+    METHOD="help"
+    shift
+    ;;
+    --minutes=*)
+    MINUTES="${i#*=}"
+    shift
+    ;;
+    --hours=*)
+    HOURS="${i#*=}"
+    shift
+    ;;
+    --days=*)
+    DAYS="${i#*=}"
+    shift
+    ;;
     -f=*|--folder=*)
     FOLDER="${i#*=}/"
     shift
     ;;
     -d|--dry-run)
     DRYRUN=" --dry-run "
-    shift
-    ;;
-    -h|--help)
-    METHOD="help"
     shift
     ;;
     *)
@@ -112,6 +171,10 @@ done
 case $METHOD in
   mount)
     mountRemote
+    exit
+    ;;
+  diff)
+    findDifferences
     exit
     ;;
   up)
