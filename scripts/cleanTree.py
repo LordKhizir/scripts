bd00ea22f79648be93cbfae3f1af096b80acde15
Toni Navarro <toni.navarro@infojobs.net>
2021-03-09 21:33:02 +0100

    New cleanTree.py - removes unwanted files... and purges sensible ones

---
+++ scripts/cleanTree.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+
+# cleandir - Clean current directory
+# Remove unwanted files
+# Remove comments
+
+folder = "."
+for subdir, dirs, files in os.walk(folder):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+        name, extension = os.path.splitext(filepath)
+        extension = extension.lower()
+
+        print(filepath)
+
+        # Files to be removed
+        if name.endswith(".DS_Store"):
+            print("\tRemoving it")
+            os.remove(filepath)
+
+        # Ascii OBJ and STL - Remove comments
+        if extension in (".obj", ".stl"):
+            print("\tChecking")
+            cleanit=False
+            try:
+                with open(filepath, "r") as f:
+                    lines = f.readlines()
+                for line in lines:
+                    if line.startswith("#"):
+                        print("\tComment found.")
+                        cleanit=True
+                        break
+            except UnicodeDecodeError:
+                # Binary file, just continue
+                continue
+
+            if cleanit:
+                print("\tCleaning")
+                with open(filepath, "w") as f:
+                    for line in lines:
+                        if line.startswith("#"):
+                            print("\t\tRemoved " + line)
+                        else:
+                            f.write(line)
+
+
