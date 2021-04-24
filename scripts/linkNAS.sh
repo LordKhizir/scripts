bcbf3d7cb22af4d2503b0ca44a023b95856a97e1
Toni Navarro <toni.navarro@infojobs.net>
2021-04-24 18:14:27 +0200

    First version - correctly compares folders

--- scripts/linkNAS.sh
+++ scripts/linkNAS.sh
@@ -1,1 +1,1 @@
-ln -s ~/Library/Containers/com.wdc.WDDesktop.WDDesktopFinderSync/Data/volumes/a759f174-4254-4b46-b836-ac2725b31e71/NAS ~/NAS
+ln -s ~/Library/Containers/com.wdc.WDDesktop.WDDesktopFinderSync/Data/volumes/22e26e1e-ff14-4040-b441-c3e8b2f62c23/NAS ~/NAS
