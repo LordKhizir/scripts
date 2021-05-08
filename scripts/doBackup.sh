30ad1747dde2956123189cacfc9a01802065ab52
Toni Navarro <toni.navarro@infojobs.net>
2021-05-08 16:05:06 +0200

    Refactor for different and specific folders

--- scripts/doBackup.sh
+++ scripts/doBackup.sh
@@ -1,1 +1,21 @@
-rsync -v --archive --progress --timeout=0 --human-readable --backup --backup-dir=OLD-FILES-TO-REMOVE --exclude=.DS_Store --exclude=._* --delete ~/NAS/Vault3D/ ~/Backup/Vault3D
+for folder in \
+  'Vault3D' \
+  'Hobby' \
+  'Cosplay' \
+  'Rulebooks and Lore' \
+  'DO NOT SHARE'
+
+do
+  echo BACKING UP ${folder}
+  source="/Users/toni.navarro/NAS/${folder}/"
+  destination="/Users/toni.navarro/Backup/${folder}"
+  echo Source: ${source}
+  echo Destination: ${destination}
+
+  rsync -v --archive --progress --timeout=0 --human-readable \
+    --backup --backup-dir=OLD-FILES-TO-REMOVE \
+    --exclude=.DS_Store --exclude=._* \
+    --delete \
+    "${source}" "${destination}"
+done
+
